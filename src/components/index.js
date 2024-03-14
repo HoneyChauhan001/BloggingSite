85ae8cba8ee13c09df0c83cff3e1930dddd57f90
Honey-Chauhan <honey.chauhan@greenstitch.io>
2024-03-15 02:02:14 +0530

    Button Input and Select component done

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Logo from "./Logo";
 import Container from "./Container";
 import LogoutBtn from "./header/LogoutBtn";
+import Button from "./Button";
 
 export {
     Logo,
     Container,
-    LogoutBtn
+    LogoutBtn,
+    Button
 }
