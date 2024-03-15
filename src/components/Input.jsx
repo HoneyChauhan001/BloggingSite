d8eb45307909ca0c66f49acd44adc3d572d46059
Honey-Chauhan <honey.chauhan@greenstitch.io>
2024-03-15 22:53:38 +0530

    Login, signup and authlayout components done

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -8,6 +8,7 @@ const Input = React.forwardRef(
         ...props
     }, ref) {
         const id = useId()
+        console.log(`ref: ${ref}`)
 
         return (
             <div className='w-full'>
--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,21 @@ import Logo from "./Logo";
 import Container from "./Container";
 import LogoutBtn from "./header/LogoutBtn";
 import Button from "./Button";
+import Input from "./Input";
+import Select from "./Select";
+import PostCard from "./PostCard";
+import Login from "./Login";
+import Signup from "./Signup";
 
 export {
     Logo,
     Container,
     LogoutBtn,
-    Button
+    Button,
+    Input,
+    Select,
+    PostCard,
+    Login,
+    Signup
+
 }
