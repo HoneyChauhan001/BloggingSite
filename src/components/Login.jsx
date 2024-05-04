f4d73c5f55d6580b2430d7a35e34858a0e93ba34
Honey-Chauhan <honey.chauhan@greenstitch.io>
2024-05-04 13:40:15 +0530

    Resolve issue with postform

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -19,6 +19,7 @@ function Login() {
             const session = await authService.login(data)
             if (session) {
                 const userData = await authService.getCurrentUser()
+                console.log('login',userData)
                 if (userData) dispatch(authLogin(userData))
                 navigate("/")
             }
